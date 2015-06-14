39076df3097ae766ba85076ac4d273b2de5095fc
josthedudeoflife <josh.chou@ses-technologies.com>
2015-06-13 20:40:31 -0400

    added south trying to get IP address

--- joins/models.py
+++ joins/models.py
@@ -5,9 +5,16 @@ from django.db import models
 
 class Join(models.Model):
 	email = models.EmailField()
+	ip_address = models.CharField(max_length=120, default='ABC')
 	timestamp = models.DateTimeField(auto_now_add = True, auto_now = False)
 	updated = models.DateTimeField(auto_now_add = False, auto_now = True)
 
 	def __unicode__(self):
 		#self.email is class variable because its declared in the class method
 		return "%s" % (self.email) 
+	#insall south
+	#ensure model is synced in db
+	#convert model to south with python manage.py convert_to_south appname
+	#make changes to model (eg add new fields: ip_address = models.CharField(max_length=120, default='ABC'))
+	#Run schemamigration: python manage.py schemamigration joins --auto
+	# run python manage.py migrate
--- joins/views.py
+++ joins/views.py
@@ -5,7 +5,21 @@ from .forms import EmailForm, JoinForm
 #this saves to database
 from .models import Join
 
+def get_ip(request):
+	try:
+		#print "hello world"
+		x_forward = request.META.get("REMOTE_ADDR")
+	# 	if x_forward:
+	# 		ip = x_forward.split(",")[0]
+	# 	else:
+	# 		ip = request.META.get("REMOTE_ADDR")
+	except:
+	 ip = ""
+	return x_forward
+	print "hello world"
+
 def home(request):
+	#print request.META.get("REMOTE_ADDR")
 	# print request.POST
 	# form = EmailForm()
 	# # form = EmailForm(request.POST or None)
@@ -15,9 +29,10 @@ def home(request):
 	form = JoinForm(request.POST or None)
 	if form.is_valid():
 		new_join = form.save(commit = False)
-		email = form.cleaned_data['email']
-		new_join_old, created = Join.objects.get_or_create(email=email)
-		# new_join.save()
+		#email = form.cleaned_data['email']
+		#new_join_old, created = Join.objects.get_or_create(email=email)
+		new_join.ip_addrees = get_ip(request)
+		new_join.save()
 	context = {"form": form}
 	template = "home.html"
 	return render(request, template, context)
